34a07bfaf31dd5d35e268eb1b475c9866744b9ce
JEYANTH-P <jeyabalan764@gmail.com>
2024-03-23 00:08:51 +0530

    Created frontend structure

---
+++ operator-frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {Button} from "@/components/ui/button"
+
+import { Route,Routes,BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={} />
+        </Routes>
+      </Router>
+      
+    </>
+  )
+}
+
+export default App
