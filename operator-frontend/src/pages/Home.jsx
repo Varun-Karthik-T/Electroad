7f939ea9cfb87b8bec5d31b846455c101e0238de
JEYANTH-P <jeyabalan764@gmail.com>
2024-03-23 13:38:02 +0530

    Operator-updated

--- operator-frontend/src/pages/Home.jsx
+++ operator-frontend/src/pages/Home.jsx
@@ -1,24 +1,14 @@
-import React from 'react'
-import axios from 'axios'
-import { Button } from '@/components/ui/button'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import {
-    Chart as ChartJS,
-    ArcElement,
-    Tooltip,
-    Legend
-} from 'chart.js'
-import { Doughnut } from 'react-chartjs-2'
-
-ChartJS.register(ArcElement, Tooltip, Legend)
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button } from '@/components/ui/button';
+import { Doughnut } from 'react-chartjs-2';
 
 const textCenter = {
     id: 'textCenter',
-    beforeDatasetsDraw (chart, args, pluginOptions) {
-        const {ctx, data} = chart;
+    beforeDatasetsDraw(chart, args, pluginOptions) {
+        const { ctx, data } = chart;
         ctx.save();
-        ctx.font ='bolder 30px sans-serif ';
+        ctx.font = 'bolder 30px sans-serif';
         ctx.fillStyle = 'black';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
@@ -26,75 +16,70 @@ const textCenter = {
         const centerY = chart.chartArea.top + chart.chartArea.height / 2;
         ctx.fillText('text', centerX, centerY);
     }
-}
+};
 
 const Home = () => {
-    const [port, setData] = useState({});
+    const [portData, setPortData] = useState([]);
 
     useEffect(() => {
-        const postData = async () => {
+        const fetchData = async () => {
             try {
-                const response = await axios.post('/', {
-                    stationid: '1'
+                const response = await axios.post('http://localhost:5000/documents_id', {
+                    "station_id": 100
                 });
                 console.log(response);
-                setData(response.data);
+                setPortData(response.data.port_documents);
             } catch (error) {
                 console.error(error);
-                alert('An error occurred while making the request');
             }
         };
-        postData();
+        fetchData();
     }, []);
-
-    const data = {
-        labels: ['Yes', 'No'],
-        datasets: [{
-            label: 'Poll',
-            data: [3, 6],
-            backgroundColor: [
-                'rgba(255,99,132,0.2)',
-                'rgba(54,162,235,0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54,162,235,1)'
-           ],
-        }]
-    }
-    const options = {}
 
     return (
         <>
-            <div className='bg-[#0f172a] h-[50px] text-[#ffffff] font-poppins flex justify-around  items-center flex-wrap '>
+            <div className='bg-[#0f172a] h-[50px] text-[#ffffff] font-poppins flex justify-around items-center flex-wrap'>
                 <div>Home</div>
                 <div>Home</div>
                 <div>Home</div>
             </div>
             <Button>Click me</Button>
-            <div className='flex justify-around  items-center gap-10 max-w-full'>
-                <div>
-                    <Doughnut
-                        data={data}
-                        options={options}
-                        height={250}
-                        width={250}
-                        plugins ={[textCenter]}
-                    ></Doughnut>
-                </div>
-                <div>
-                <Doughnut
-                    data={data}
-                    options={options}
-                    height={250}
-                    width={250}
-                    plugins ={[textCenter]} // Add this line
-                ></Doughnut>
-                </div>
-                
+            <div className='flex justify-around items-center gap-10 max-w-full'>
+                {portData.map((port, index) => (
+                    <div key={index}>
+                        <Doughnut
+                            data={{
+                                labels: Object.keys(port),
+                                datasets: [{
+                                    label: 'Port Data',
+                                    data: Object.values(port),
+                                    backgroundColor: [
+                                        'rgba(255,99,132,0.2)',
+                                        'rgba(54,162,235,0.2)',
+                                        'rgba(255,206,86,0.2)',
+                                        'rgba(75,192,192,0.2)',
+                                        'rgba(153,102,255,0.2)',
+                                    ],
+                                    borderColor: [
+                                        'rgba(255,99,132,1)',
+                                        'rgba(54,162,235,1)',
+                                        'rgba(255,206,86,1)',
+                                        'rgba(75,192,192,1)',
+                                        'rgba(153,102,255,1)',
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            }}
+                            options={{}}
+                            height={250}
+                            width={250}
+                            plugins={[textCenter]}
+                        />
+                    </div>
+                ))}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
